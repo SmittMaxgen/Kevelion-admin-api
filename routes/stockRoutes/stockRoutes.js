ec95810daaf1f781e34d56a64ad820396e09cfb2
Your Name <your.email@example.com>
2026-02-12 09:54:44 +0530

    stockRoutes updated !

--- routes/stockRoutes/stockRoutes.js
+++ routes/stockRoutes/stockRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/stock", createStock); // POST
 router.get("/stock", getAllStock); // GET all
 router.get("/stock/:seller_id", getStockBySeller); // GET by seller
-router.patch("stock/:id", updateStock); // PATCH
-router.delete("stock/:id", deleteStock); // DELETE
+router.patch("/stock/:id", updateStock); // PATCH
+router.delete("/stock/:id", deleteStock); // DELETE
 
 export default router;
